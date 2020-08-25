ac3124ca452c2eab41348adb8851f16eb1f96831
Dennis <yowidin@gmail.com>
2020-08-25 17:38:17 +0200

    Fixed CI script
    Bumped up API level for the 64-bit builds

--- .ci/build-android.py
+++ .ci/build-android.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     builder.add(settings={"arch": "armv7", "os.api_level": "16"})
     builder.add(settings={"arch": "armv8", "os.api_level": "21"})
     builder.add(settings={"arch": "x86", "os.api_level": "16"})
-    builder.add(settings={"arch": "x86_64", "os.api_level": "16"})
+    builder.add(settings={"arch": "x86_64", "os.api_level": "21"})
     builder.run()
