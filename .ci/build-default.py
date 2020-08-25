dd47af8c4ea122d518a22169d35d33099a26e17e
Dennis <yowidin@gmail.com>
2020-08-25 19:16:29 +0200

    Update build-default.py
    Use C++17 as default for native builds

--- .ci/build-default.py
+++ .ci/build-default.py
@@ -2,5 +2,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add()
+    builder.add(settings={"compiler.cppstd": "17"})
     builder.run()
